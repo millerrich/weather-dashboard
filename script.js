332b81b9eb88dba2e88cbfdbeaa2228c1516b964
Miller Rich <william.miller.rich@gmail.com>
2020-08-30 12:41:04 -0500

    add ajax query, set variables for temp in fahrenheit and celsius, set variable for image of icon represnting the current weather in the area you search, remove slim jquery link and add original

--- script.js
+++ script.js
@@ -1,4 +1,38 @@
 // api key 242726fc7e61ebfc759b17352fed0b73
 
-var queryURL = "api.openweathermap.org/data/2.5/weather?q= + city + &appid=242726fc7e61ebfc759b17352fed0b73"
-var city = document.getElementById("cityInput");
+
+// on click for search
+$('#searchBtn').on('click', function(event) {
+    event.preventDefault();
+    
+    var search = $('#search').val();
+    
+    
+    var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + search + "&appid=242726fc7e61ebfc759b17352fed0b73"
+    console.log(search);
+    console.log(queryURL);
+    
+    // ajax query
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+    }).then(function(response) {
+        console.log(response);
+        var tempF = (response.main.temp - 273.15) * 9/5 + 32;
+        console.log(tempF);
+        var tempC = response.main.temp -273.15;
+        console.log(tempC);
+        var weatherArray = [];
+        var icon = response.weather[0].icon
+        console.log(icon); 
+        var weatherIcon = $('<img>')
+        weatherIcon.attr('src', "http://openweathermap.org/img/wn/" + icon + "@2x.png")
+        $('.article-container').append(weatherIcon);
+        var testObject = {
+            one: "throw",
+            two: "Tree",
+            three: "nowie"
+        }
+        console.log(testObject);
+    });
+});
