d86bc3ed03c36f41b4575807496248c579a6c61d
Miller Rich <william.miller.rich@gmail.com>
2020-08-29 20:59:36 -0500

    put together page layout in index.html and basic styling in style.css

--- script.js
+++ script.js
@@ -1,1 +1,4 @@
 // api key 242726fc7e61ebfc759b17352fed0b73
+
+var queryURL = "api.openweathermap.org/data/2.5/weather?q= + city + &appid=242726fc7e61ebfc759b17352fed0b73"
+var city = document.getElementById("cityInput");
