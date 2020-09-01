23c210d9ba183d745c3dcf14879c59beeed70732
Miller Rich <william.miller.rich@gmail.com>
2020-09-01 15:39:44 -0500

    add catch method to uvQuery function which will append a message if the uv index api call fails due to CORS error

--- script.js
+++ script.js
@@ -12,6 +12,11 @@ var tempF;
 var tempC;
 var humidity;
 var wind;
+var lat;
+var lon;
+var uv;
+var uvURL;
+var oneCallURL;
 // initialize
 init();
 
@@ -22,15 +27,15 @@ function init() {
         search = searchHistory[0];
         ajaxQuery();
     } else {
-    // storeEvents();
-    renderEvents();
+        // storeEvents();
+        renderEvents();
     }
 };
 
 function renderEvents() {
     
     $('.search-history').empty();
-
+    
     for (var i = 0; i < searchHistory.length; i++) {
         index = searchHistory[i];
         $('.search-history').append(index);
@@ -55,8 +60,6 @@ $('#searchBtn').on('click', function(event) {
 });
 
 function ajaxQuery() {
-    var lat;
-    var lon;
     
     console.log(searchHistory);
     storeEvents();
@@ -65,11 +68,10 @@ function ajaxQuery() {
     var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + search + "&appid=242726fc7e61ebfc759b17352fed0b73"
     var fiveDayQuery = "https://api.openweathermap.org/data/2.5/forecast?q=" + search + "&appid=242726fc7e61ebfc759b17352fed0b73"
     
-    // var uvURL = "http://api.openweathermap.org/data/2.5/uvi?appid=242726fc7e61ebfc759b17352fed0b73&lat=" + lat + "&lon=" + lon
     // console.log(search);
     // console.log(queryURL);
-
-     // ajax query
+    
+    // ajax query
     $.ajax({
         url: queryURL,
         method: "GET"
@@ -107,8 +109,10 @@ function ajaxQuery() {
         console.log("lat: " + lat);
         lon = response.coord.lon;
         console.log("lon: " + lon);
-
-        append();
+        uvURL = "http://api.openweathermap.org/data/2.5/uvi?appid=242726fc7e61ebfc759b17352fed0b73&lat=" + lat + "&lon=" + lon
+        // oneCallURL = "https://api.openweathermap.org/data/2.5/onecall?lat=" + lat + "&lon=" + lon + "&exclude=hourly,minutely&appid=242726fc7e61ebfc759b17352fed0b73"
+        // append();
+        uvQuery();
         
     });
     
@@ -121,20 +125,30 @@ function ajaxQuery() {
         console.log(cityName);
         // var icon = response.list[].weather[].icon
     })
-    // $.ajax({
-    //     query: uvURL,
-    //     method: "GET"
-    // }).then(function(uvi) {
-    //     console.log(uvi);
-    // })
 };
-    
-    // append data to current-weather section
-    function append() {
+function uvQuery() {
+    console.log(uvURL);
+    $.ajax({
+        query: uvURL,
+        method: "GET"
+    }).then(function(uvi) {
+        console.log(uvi.value);
+        uv = uvi.value;
+    }).catch(function(err) {
+        console.log(err);
+        uv = "N/A";
+    }).then(function() {
+        append();
+    })
+};
+// append data to current-weather section
+function append() {
+    $('#city-date, #icon, #temp, #humidity, #wind, #uv').empty();
     
     $('#city-date').append(city + ", " + month + " " + date);
     $('#icon').append(weatherIcon);
     $('#temp').append("Temperature: " + tempF + "°F");
     $('#humidity').append("Humidity: " + humidity + "%");
     $('#wind').append("Windspeed: " + wind + " mph");
+    $('#uv').append("UV Index: " + uv);
     };
