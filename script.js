649ea75c196a16dd9b268b70675bafeaf5b9a57a
Miller Rich <william.miller.rich@gmail.com>
2020-09-02 09:54:14 -0500

    fix five day forecast, fix search history buttons

--- script.js
+++ script.js
@@ -19,13 +19,12 @@ var uvURL;
 var oneCallURL;
 // five day
 var art = document.getElementById(1);
-var weather5 = $('<img>')
 // initialize
 init();
 
 function init() {
     var storedSearch = JSON.parse(localStorage.getItem("searchHistory"));
-    if(storedSearch) {
+    if (storedSearch) {
         searchHistory = storedSearch;
         search = searchHistory[0];
         ajaxQuery();
@@ -36,18 +35,18 @@ function init() {
 };
 
 function renderEvents() {
-    
+
     $('.search-history').empty();
-    
+
     for (var i = 0; i < searchHistory.length; i++) {
         index = searchHistory[i];
         var btn = $('<button>');
-        btn.attr("class", "btn btn-outline-secondary historyBtn")
-        btn.attr("type", "button")
-        btn.attr("value", index)
-        btn.text(index)
+        btn.attr("class", "btn btn-outline-secondary historyBtn");
+        btn.attr("type", "button");
+        btn.attr("value", index);
+        btn.text(index);
         $('.search-history').append(btn);
-        
+
     }
 }
 
@@ -57,7 +56,7 @@ function storeEvents() {
 
 
 // on click for search
-$('#searchBtn').on('click', function(event) {
+$('#searchBtn').on('click', function (event) {
     event.preventDefault();
     if ($('#search').val() === "") {
         alert("enter valid city")
@@ -69,7 +68,7 @@ $('#searchBtn').on('click', function(event) {
 });
 
 // localstorage buttons
-$('.historyBtn').on('click', function(event) {
+$('.search-history').on('click', '.historyBtn', function (event) {
     event.preventDefault();
     console.log("click");
     search = $(this).val();
@@ -78,22 +77,22 @@ $('.historyBtn').on('click', function(event) {
 });
 
 function ajaxQuery() {
-    
+
     console.log(searchHistory);
     storeEvents();
     renderEvents();
-    
+
     var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + search + "&appid=242726fc7e61ebfc759b17352fed0b73"
     var fiveDayQuery = "https://api.openweathermap.org/data/2.5/forecast?q=" + search + "&appid=242726fc7e61ebfc759b17352fed0b73"
-    
+
     // console.log(search);
     // console.log(queryURL);
-    
+
     // ajax query
     $.ajax({
         url: queryURL,
         method: "GET"
-    }).then(function(response) {
+    }).then(function (response) {
         console.log(response);
         // city name
         city = response.name;
@@ -107,14 +106,14 @@ function ajaxQuery() {
         console.log(date);
         // current weather icon
         var icon = response.weather[0].icon
-        console.log(icon); 
+        console.log(icon);
         // var weatherIcon = $('<img>')
         weatherIcon.attr('src', "http://openweathermap.org/img/wn/" + icon + "@2x.png")
         // temp F
-        tempF = Math.round((response.main.temp - 273.15) * 9/5 + 32);
+        tempF = Math.round((response.main.temp - 273.15) * 9 / 5 + 32);
         console.log(tempF);
         // temp C
-        tempC = Math.round(response.main.temp -273.15);
+        tempC = Math.round(response.main.temp - 273.15);
         console.log(tempC);
         // humidity
         humidity = response.main.humidity;
@@ -127,50 +126,55 @@ function ajaxQuery() {
         console.log("lat: " + lat);
         lon = response.coord.lon;
         console.log("lon: " + lon);
-        uvURL = "http://api.openweathermap.org/data/2.5/uvi?appid=242726fc7e61ebfc759b17352fed0b73&lat=" + lat + "&lon=" + lon
-        // oneCallURL = "https://api.openweathermap.org/data/2.5/onecall?lat=" + lat + "&lon=" + lon + "&exclude=hourly,minutely&appid=242726fc7e61ebfc759b17352fed0b73"
-        // append();
+        uvURL = "http://api.openweathermap.org/data/2.5/uvi?appid=242726fc7e61ebfc759b17352fed0b73&lat=" + lat + "&lon=" + lon;
         uvQuery();
-        
+
     });
-    
+
     $.ajax({
         url: fiveDayQuery,
         method: "GET"
-    }).then(function(response) {
+    }).then(function (response) {
         console.log(response)
-        
-        for (var i = 0; i < response.list.length; i++) 
-        if (response.list[i].dt_txt.indexOf("15:00:00")!==-1) {
-            
-            
-            var $container = $('#article-container');
-            var $art = $('article');
-            // container.empty()
+        var $div = $('<div class="fiveDiv">');
+        var $container = $('.article-container');
 
-            
-            var date = response.list[i].dt_txt;
-            console.log(date);
-            
-            var get = response.list[i];
-            var icon5 = get.weather[0].icon;
-            weather5.attr('src', "http://openweathermap.org/img/wn/" + icon5 + "@2x.png")
-            console.log(icon5);
-            
-            var tempF5 = Math.round((get.main.temp - 273.15) * 9/5 + 32);
-            console.log("Temperature: " + tempF5);
-            
-            var humidity5 = get.main.humidity;
-            console.log("Humidity: " + humidity5);
-            
-            $art.append("<br>" + "Date: " + date + "<br>" + "Temperature: " + tempF5 + "<br>" + "Humidity: " + humidity5 + "<br>");
-            $art.append(weather5);
-            
-            $container.append(art);
+        for (var i = 0; i < response.list.length; i++) {
+            if (response.list[i].dt_txt.indexOf("15:00:00") !== -1) {
 
-            
-            
-        } 
+
+                var $art = $('<article>');
+                var weather5 = $('<img>')
+                
+                console.log($art);
+
+                var date = moment(response.list[i].dt_txt).format("ddd, MMM D, YYYY");
+                console.log(date);
+
+                var get = response.list[i];
+                var icon5 = get.weather[0].icon;
+                weather5.attr('src', "http://openweathermap.org/img/wn/" + icon5 + "@2x.png")
+                console.log(icon5);
+
+                var tempF5 = Math.round((get.main.temp - 273.15) * 9 / 5 + 32);
+                console.log("Temperature: " + tempF5);
+
+                var humidity5 = get.main.humidity;
+                console.log("Humidity: " + humidity5);
+
+                $art.append("<p>Date:  " + date + "</p>");
+                $art.append("<p>Temperature: " + tempF5 + "</p>");
+                $art.append("<p>Humidity: " + humidity5 + "</p>");
+
+                $art.append(weather5);
+
+                $div.append($art);
+
+
+
+            }
+        } $container.html($div);
+
     })
 };
 function uvQuery() {
@@ -178,24 +182,24 @@ function uvQuery() {
     $.ajax({
         query: uvURL,
         method: "GET"
-    }).then(function(uvi) {
+    }).then(function (uvi) {
         console.log(uvi.value);
         uv = uvi.value;
-    }).catch(function(err) {
+    }).catch(function (err) {
         console.log(err);
         uv = "N/A";
-    }).then(function() {
+    }).then(function () {
         append();
     })
 };
 // append data to current-weather section
 function append() {
     $('#city-date, #icon, #temp, #humidity, #wind, #uv').empty();
-    
+
     $('#city-date').append(city + ", " + month + " " + date);
     $('#icon').append(weatherIcon);
     $('#temp').append("Temperature: " + tempF + "°F");
     $('#humidity').append("Humidity: " + humidity + "%");
     $('#wind').append("Windspeed: " + wind + " mph");
     $('#uv').append("UV Index: " + uv);
-    };
+};
